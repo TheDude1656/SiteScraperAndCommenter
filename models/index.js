ad6d91131cc28d9d60f5244067049980f35c08fa
Kyle Atchison <katchison@gmail.com>
2017-10-27 21:27:56 -0500

    changes

--- models/index.js
+++
@@ -1,7 +0,0 @@
-// Exporting an object containing all of our models
-
-module.exports = {
-    Title: require("./Title"),
-    Description: require("./Description"),
-    Link: require("./Link")
-};
